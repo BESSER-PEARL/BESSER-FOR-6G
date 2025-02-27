0c562e9046bb1ae38717d28adb89488089551dd9
ArmSL <scorp-arm@hotmail.com>
2025-02-27 13:12:19 +0100

    Add YANG parser implementation to parse JSON files and create domain models

--- generated_models/_3gpp_5g_common_yang_types.py
+++
@@ -1,8 +0,0 @@
-from besser.BUML.metamodel.structural import (
-    Class, Property, DomainModel,
-    StringType, IntegerType
-)
-from ._3gpp_common_yang_types import DistinguishedName
-
-# Create domain model
-domain_model = DomainModel(name="_3gpp_5g_common_yang_types")
